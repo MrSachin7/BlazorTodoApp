967071b2200f2cbd86c5c0eb30d427274416160f
MrSachin7 <82201769+MrSachin7@users.noreply.github.com>
2022-03-07 13:01:03 +0100

    TodoApp in Blazor

---
+++ Domain/Contracts/ITodoHome.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+
+namespace Domain.Contracts; 
+
+public interface ITodoHome {
+    public Task<ICollection<Todo>> GetAsync();
+    public Task<Todo> GetById(int id);
+    public Task<Todo> AddAsync(Todo todo);
+    public Task DeleteAsync(int id);
+    public Task UpdateAsync(Todo todo);  
+}
---
+++ FileData/DataAccess/TodoFileDAO.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Contracts;
+using Domain.Models;
+
+namespace FileData.DataAccess; 
+
+public class TodoFileDAO: ITodoHome {
+    private FileContext fileContext;
+
+    public TodoFileDAO(FileContext fileContext) {
+        this.fileContext = fileContext;
+    }
+
+    public async Task<ICollection<Todo>> GetAsync() {
+        ICollection<Todo> todos = fileContext.Todos;
+        return todos;
+    }
+
+    public async Task<Todo> GetById(int id) {
+        return fileContext.Todos.First(todo => todo.Id == id);
+    }
+
+    public async Task<Todo> AddAsync(Todo todo) {
+        int largestId = fileContext.Todos.Max(t => t.Id);
+        int nextId = largestId + 1;
+        todo.Id = nextId;
+        fileContext.Todos.Add(todo);
+        fileContext.SaveChanges();
+        return todo;  
+    }
+
+    public  Task DeleteAsync(int id) {
+        Todo todelete = fileContext.Todos.First(todo => todo.Id == id);
+        fileContext.Todos.Remove(todelete);
+        fileContext.SaveChanges();
+        return Task.CompletedTask;
+    }
+
+    public Task UpdateAsync(Todo todo) {
+        Todo toUpdate = fileContext.Todos.First(t => t.Id == todo.Id);
+        toUpdate.IsCompleted = todo.IsCompleted;
+        toUpdate.OwnerId = todo.OwnerId;
+        toUpdate.Title = todo.Title;
+        fileContext.SaveChanges();
+        return Task.CompletedTask;
+
+    }
+}
